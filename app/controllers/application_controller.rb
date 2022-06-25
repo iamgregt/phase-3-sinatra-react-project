c75991c1e718bf66f1ac9c3a6fd1e98ee4ae38fb
GregT <taylor.gregory2@gmail.com>
2022-06-24 21:58:32 -0600

    1st commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,46 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/employees" do
+    Employee.all.to_json
+  end
+
+  get "/customers" do
+    Customer.all.to_json
+  end
+
+  post "/employees" do
+    employee = Employee.create(
+      name: params[:name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      address: params[:address]
+    )
+    employee.to_json
+  end
+
+  post "/customers" do
+    customer = Customer.create(
+      name: params[:name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      address: params[:address]
+    )
+    customer.to_json
+  end
+
+  delete "/customers/:id" do
+    customer = Customer.find(params[:id])
+    customer.destroy
+    customer.to_json
+  end
+
+  patch "/customers/:id" do
+    customer = Customer.find(params[id])
+    customer.update(
+      address: params[:address]
+    )
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,52 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+Customer.create([
+  {
+    name: "Jennifer Taylor",
+    email: "jtaylor@gmail.com",
+    phone_number: 9013572800,
+    address: "4343 Windsor Rd"
+  },
+  {
+    name: "Greg York",
+    email: "gyork@gmail.com",
+    phone_number: 9013234820,
+    address: "1560 Quincy Rd"
+  },
+  {
+    name: "Tony Roberts",
+    email: "troberts@gmail.com",
+    phone_number: 9014504545,
+    address: "1766 England Street"
+  },
+  {
+    name: "Justin Jones",
+    email: "jjones@gmail.com",
+    phone_number: 9013389762,
+    address: "4711 Second Street"
+  },
+  {
+    name: "Tory Washington",
+    email: "twashington@gmail.com",
+    phone_number: 9015961182,
+    address: "8777 Heaven Ave"
+  }
+])
+
+Employee.create([
+{
+    name: "Jamaal Calicutt",
+    email: "jcalicutt@homeshine901.com",
+    phone_number: 9016262912,
+    address: "5921 Favell Rd"
+},
+{
+    name: "Kevin Miller",
+    email: "kmiller@homeshine901.com",
+    phone_number: 9013519348,
+    address: "9821 Bluebird St"
+}
+])
+
 
 puts "✅ Done seeding!"
